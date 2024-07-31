92f9b9607f842553867178c3325f34b70b03d28d
lys <lyscalefi@gmail.com>
2024-07-30 21:11:37 -0300

    add initial structure

---
+++ property-app-uikit/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  property-app-uikit
+//
+//  Created by Amaryllis Rosemaria Baldrez Calefi on 30/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
